2bb358e011088b79d66a6db9f041b3c9f52d2ed9
kit <pacificatore403@gmail.com>
2026-02-09 10:50:04 +0700

    fourth commit

---
+++ general/kit/fourth/fourth.py
@@ -0,0 +1,37 @@
+a = 33
+a += 2
+print(a)
+b = 47
+b -= 6
+print(b)
+c =27
+c *= 2
+print(c)
+d = 666
+d /= 3
+print(d)
+f = 5
+f %= 2
+print(f)
+g = 11
+g **= 3
+print(g)
+h = 76
+h //= 12
+print(h)
+kit = 'kot'
+kit += ' Kit'
+print(kit)
+
+k = 2
+l = 2
+print(k is l) #одна и та же ячейка памяти
+print(k == l) #одно и то же значение
+print(id(h))
+
+user_beer = input('What is your favourite beer?')
+print(user_beer,  'is your favourite beer')
+
+user_age = int(input('What is your age?'))
+print(user_age + 200)
+
